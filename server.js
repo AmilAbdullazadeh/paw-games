8dc7b3b1f515927bba2a78e59d10301aa6aa50a3
Amil Abdullazadeh <amil.abdullazadeh@vabiss.com>
2023-02-26 20:41:49 +0400

    reversed

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const jsonServer = require("json-server");
-const cors = require("cors");
+// const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
@@ -7,20 +7,15 @@ const middlewares = jsonServer.defaults();
 const PORT = 5555;
 
 server.use(middlewares);
-// server.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-// for cors error
-server.use(
-  cors({
-    origin: "https://paw-games.vercel.app",
-  })
-);
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+// server.use(cors());
 server.use(router);
 
 server.listen(PORT, () => {
